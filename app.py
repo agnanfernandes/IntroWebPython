be1325001315093fe03f91fcc3f5ba14a84e928a
agnanfernandes <agnanfernandes@gmail.com>
2022-11-23 18:59:24 +0000

    Manipulação de variáveis no html e instruções if e for

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "banco.bd"
+SECRET_KEY = "1234"
 
 app = Flask("Olá") #para utilizar os recursos do framework por essa variável
 
-"""app.route: metodo que permite rotas para serem acessadas
-    e passa o nome da rota que "/" é o diretório raiz, para outros diretorios,
-    especificar nomes."""
-@app.route("/alunos") 
-def alunos():
-    return render_template("hello.html")
+app.config.from_object(__name__)
 
-    
+def conectar():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def before_first_request():
+    g.bd = conectar()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.bd.close()
+
+@app.route("/") 
+def alunos():
+    nomeUsuario = ["Danilo", "Caio", "Guilherme", None, "Cassio"]
+    return render_template("hello.html", nome=nomeUsuario)
+
+ 
